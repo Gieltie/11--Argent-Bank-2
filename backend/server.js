b82b6e1e65cc9857471183cb0be55a093f076944
Gael Hofman <Gieltie@outlook.fr>
2023-11-06 01:27:32 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,52 @@
-const express = require('express')
-const dotEnv = require('dotenv')
-const cors = require('cors')
-const swaggerUi = require('swagger-ui-express')
-const yaml = require('yamljs')
-const swaggerDocs = yaml.load('./swagger.yaml')
-const dbConnection = require('./database/connection')
+const path = require("path");
+const express = require("express");
+const dotEnv = require("dotenv");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("yamljs");
+const swaggerDocs = yaml.load("./swagger.yaml");
+const dbConnection = require("./database/connection");
 
-dotEnv.config()
+dotEnv.config();
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Connect to the database
-dbConnection()
+dbConnection();
 
 // Handle CORS issues
-app.use(cors())
+app.use(cors());
 
 // Request payload middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Handle custom routes
-app.use('/api/v1/user', require('./routes/userRoutes'))
+app.use("/api/v1/user", require("./routes/userRoutes"));
 
-// API Documentation
-if (process.env.NODE_ENV !== 'production') {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+// Serve Frontend
+if (process.env.NODE_ENV !== "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "dist", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
 }
 
-app.get('/', (req, res, next) => {
-  res.send('Hello from my Express server v2!')
-})
+// API Documentation
+if (process.env.NODE_ENV !== "production") {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+}
+
+app.get("/", (req, res, next) => {
+  res.send("Hello from my Express server v2!");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`)
-})
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
